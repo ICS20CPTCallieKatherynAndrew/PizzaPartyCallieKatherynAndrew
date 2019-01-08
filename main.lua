d34a5caceeb98738404c4540abb5da4f0625b579
AndrewKuekam17 <43419144+AndrewKuekam17@users.noreply.github.com>
2019-01-08 13:18:05 -0500

    .

--- main.lua
+++ main.lua
@@ -12,7 +12,7 @@ local composer = require( "composer" )
 
 -----------------------------------------------------------------------------------------
 
-composer.gotoScene( "level2_screen" )
+composer.gotoScene( "splash_screen" )
 
 
 
