6cc4dfa6cb0aa62bfd190d0e662f9714fbcbfbd9
AndrewKuekam17 <43419144+AndrewKuekam17@users.noreply.github.com>
2019-01-17 13:22:16 -0500

    Updated

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 
-composer.gotoScene( "level3_screen")
+composer.gotoScene( "level2_screen")
 
 
 
